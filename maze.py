36b496dbc6c7587040ee2552dbdb2d76f1852549
Sander van den Hoogen <svdhoogen1999@gmail.com>
2020-08-30 20:36:35 +0200

    Restarted writing maze generation code
    Maze now consists of list of maze tiles, each holding their own location, walls and visitded property. When generating maze we now select a random starting tile and from there we get and select a neighbor. Thats where i stoped for now

--- maze.py
+++ maze.py
@@ -2,7 +2,7 @@
 
 from dataclasses import dataclass
 from datetime import datetime
-from random import random, seed, getrandbits
+from random import random, seed, getrandbits, choice, randint
 
 @dataclass
 class Point:
@@ -16,13 +16,21 @@ class Line:
     start: Point
     end: Point
 
+@dataclass
+class Tile:
+    """A maze tile object, has a point and visited property"""
+    loc: Point
+    visited: None
+    north_enabled: None
+    east_enabled: None
+    south_enabled: None
+    west_enabled: None
+
 class Maze():
     """The maze object itself"""
     wall_char = None
     space_char = None
-    entrance_x = None
-    exit_x = None
-    maze_body = None
+    maze_tiles = None
     width = None
     height = None
 
@@ -34,115 +42,85 @@ class Maze():
         self.width = width
         self.height = height
 
-        self.maze_body = [[False for x in range(self.width)]for y in range(self.height)]
+        # Init maze tiles
+        self.maze_tiles = [[Tile(Point(x, y), False, True, True, True, True) for x in range(self.width)]for y in range(self.height)]
 
     def print_walls(self):
-        """Prints current maze to screen"""        
-        print(''
-                .join([(self.space_char if x == self.entrance_x else self.wall_char)
-                for x in range(self.width + 2)]))
+        """Prints current maze to console"""
+        
+        # Row below is a row without tiles but only walls only calculated on the final row of tileset
+        row_below = ''
 
-        for row in self.maze_body:
-            print(
-                self.wall_char
-                + ''.join([(self.space_char if not element else self.wall_char)for element in row])
-                + self.wall_char)
+        for row in self.maze_tiles:
+            # Row above is a row without tiles but only walls
+            row_above = ''
+            # Current row is a row with tiles and walls
+            current_row = ''
 
-        print(''
-                .join([(self.space_char if x == self.exit_x else self.wall_char)
-                for x in range(self.width + 2)]))
+            # Loop through each row in maze tiles
+            for element in row:
+                row_above += self.wall_char + (self.wall_char if element.north_enabled else self.space_char)
+                current_row += (self.wall_char if element.west_enabled else self.space_char) + self.space_char
 
-    def set_entrance(self, entrance_x):
-        """Sets entrance for maze. This will be at top only for now"""
-        if self.__validate_opening(entrance_x):
-            self.entrance_x = entrance_x
+                # Final column in tiles
+                if (element.loc.pos_y == self.height - 1):
+                    row_below += self.wall_char + (self.wall_char if element.south_enabled else self.space_char)
 
-    def set_exit(self, exit_x):
-        """Sets exit for maze. This will be at bottom only for now"""
-        if self.__validate_opening(exit_x):
-            self.exit_x = exit_x
+                # Final char of row, add east wall
+                if (element.loc.pos_x == self.width - 1):
+                    row_above += self.wall_char
+                    current_row += self.wall_char if element.east_enabled else self.space_char
 
-    def __validate_opening(self, pos_x):
-        """Validates a to check whether it conforms to maze bounds"""
-        try:
-            if pos_x < 1 or pos_x > self.width:
-                print("ERROR: Opening x is out of range! Couldn't add opening!")
-                return False
+                    # Add wall to row_below if final column
+                    if (element.loc.pos_y == self.height - 1):
+                        row_below += self.wall_char
+            
+            # Print the row above and current row
+            print(row_above)
+            print(current_row)
 
-            return True
-
-        except TypeError:
-            print("ERROR: Invalid opening x argument! Couldn't add opening!")
-
-    def add_wall_horizontal(self, pos_x, pos_y, width):
-        """Adds a horizontal wall from start to end x, at y"""
-        wall = Line(Point(pos_x, pos_y), Point(pos_x + width, pos_y))
-
-        if self.__validate_wall(wall):
-            for pos_x in range(wall.start.pos_x, wall.end.pos_x):
-                self.maze_body[wall.start.pos_y][pos_x] = True
-
-    def add_wall_vertical(self, pos_x, pos_y, height):
-        """Adds a vertical wall from start to end y, at x"""
-        wall = Line(Point(pos_x, pos_y), Point(pos_x, pos_y + height))
-
-        if self.__validate_wall(wall):
-            for pos_y in range(wall.start.pos_y, wall.end.pos_y):
-                self.maze_body[pos_y][wall.start.pos_x] = True
-
-    def __validate_wall(self, wall):
-        """Validates a wall to check whether it conforms to maze bounds"""
-        try:
-            if wall.start.pos_x < 0 or wall.end.pos_x > self.width or wall.start.pos_x > wall.end.pos_x:
-                print("ERROR: Pos x is out of bounds! Couldn't add wall! Start: {}, end: {}"
-                        .format(wall.start.pos_x, wall.end.pos_x))
-                return False
-
-            if wall.start.pos_y < 0 or wall.start.pos_y > self.height or wall.start.pos_y > wall.end.pos_y:
-                print("ERROR: Pos y is out of bounds! Couldn't add wall! Start: {}, end: {}"
-                        .format(wall.start.pos_y, wall.end.pos_y))
-                return False
-
-            if wall.start.pos_x == wall.end.pos_x and wall.start.pos_y == wall.end.pos_y:
-                print ("ERROR: Start and end position is identical! Couldn't add wall!")
-                return False
-
-            return True
-
-        except AttributeError:
-            print("ERROR: Invalid line object! Couldn't add wall!")
-        except TypeError:
-            print("ERROR: Invalid position arguments! Couldn't add wall!")
-
+        # Print the final row
+        print (row_below)
+    
     def generate_maze(self):
         """Generates a maze randomly"""
 
+        # Make randomization random by seeding it
         seed(datetime.now())
 
-        for y in range(self.height):
-            for x in range(self.width):
-                # Entrance/ exit tile always empty
-                if y is 0 and x == self.entrance_x - 1 or y is self.height - 1 and x == self.exit_x - 1:
-                    continue
+        tile = choice(choice(self.maze_tiles))
 
-                top_taken = y == 0 or self.maze_body[y-1][x]
-                left_taken = x == 0 or self.maze_body[y][x-1]
-                top_left_taken = x == 0 or y == 0 or self.maze_body[y-1][x-1]
-                top_right_taken = x == self.width-1 or y == 0 or self.maze_body[y-1][x+1]
+        neighbor_tiles = []
 
-                if not top_taken or not left_taken:
-                    self.place_wall_random(x,y)
+        if (tile.loc.pos_x != 0):
+            neighbor_tiles.append(self.maze_tiles[tile.loc.pos_y][tile.loc.pos_x - 1])
+
+        if (tile.loc.pos_y != 0):
+            neighbor_tiles.append(self.maze_tiles[tile.loc.pos_y - 1][tile.loc.pos_x])
+
+        if (tile.loc.pos_x != self.width - 1):
+            neighbor_tiles.append(self.maze_tiles[tile.loc.pos_y][tile.loc.pos_x + 1])
+
+        if (tile.loc.pos_y != self.height - 1):
+            neighbor_tiles.append(self.maze_tiles[tile.loc.pos_y + 1][tile.loc.pos_x])
+
+        # Todo: Check visited tiles
+
+        newTile = choice(neighbor_tiles)
+
+        newTile.north_enabled = False
+        newTile.south_enabled = False
+
+        print(neighbor_tiles)
+
+
 
     def place_wall_random(self, x, y):
         """Places a wall randomly at x and y"""
         if getrandbits(1):
-            self.maze_body[y][x] = True
+            self.maze_tiles[y][x] = True
 
-MAZE = Maze('#', ' ', 15, 15)
-
-MAZE.set_entrance(2)
-
-MAZE.set_exit(8)
+MAZE = Maze('#', '.', 2, 2)
 
 MAZE.generate_maze()
 
